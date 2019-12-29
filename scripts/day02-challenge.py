c5b5a1fdda6b1f655db1e79f9eab90c2a606b309
Dave <dm@Daves-iMac.lan>
2019-12-29 10:11:48 -0500

    finished up adding functions to the PyBite.  Having issues making the solution work from PyBites using list comprehensions.  Will need to isolate in a separate file and test.  This will determine whether I have issues in my current script somewhere unbeknownst to me :0)

--- scripts/day02-challenge.py
+++ scripts/day02-challenge.py
@@ -8,12 +8,69 @@ from datetime import timedelta
 log = "day02-challenge.log"
 with open (log, "r") as f:
     loglines=f.readlines()
+
+####################################
+# Basic script not using functions #
+####################################
+
 new_list = []
-shutdown = "Shutdown initiated"
+SHUTDOWN_EVENT = "Shutdown initiated"
 for line in loglines:
-    if shutdown in line:
+    if SHUTDOWN_EVENT in line:
         date_str=line.split(" ")[1]
         new_list.append((datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")))
 
 print(new_list[1] - new_list[0])
+print("")
+
+###################
+# Using Functions #
+###################
+
+
+#def convert_to_datetime_01(line):
+#    datestring=line.split()[1]
+#    return datetime.strptime(datestring, "%Y-%m-%dT%H:%M:%S")
+
+#def time_between_shutdowns_01(loglines):
+#    newer_list= []
+#    for line in loglines:
+#        if SHUTDOWN_EVENT in line:
+#            newer_list.append(convert_to_datetime_01(line))
+#    return max(newer_list) - min(newer_list)
+
+
+#print(time_between_shutdowns_01(loglines))
+#print("")
+
+
+
+############################################
+# Solution from PyBites with comprehension #
+# lists, better variable naming, etc       #
+############################################
+
+def convert_to_time(lines):
+    timestamp = line.split()[1]
+    date_str = '%Y-%m-%dT%H:%M:%S'
+    print("timestamp is ", timestamp)
+    return datetime.strptime(timestamp, date_str)
+
+
+
+def time_between_shutdowns(loglines):
+    shutdown_entries=[line for line in loglines if SHUTDOWN_EVENT in line]
+    print(shutdown_entries)
+    shutdown_times=[convert_to_time(event) for event in shutdown_entries]
+    print(shutdown_times)
+    return max(shutdown_times) - min(shutdown_times)
+
+
+print(time_between_shutdowns(loglines))
+
+
+
+
+
+
 
