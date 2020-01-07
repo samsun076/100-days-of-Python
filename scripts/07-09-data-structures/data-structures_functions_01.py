42aee884b2b6c9fe9c9356a5549ea8020348d3df
Dave Marcinowski <davemarcinowski@gmail.com>
2020-01-07 05:26:18 -0500

    adding functions to challenge

---
+++ scripts/07-09-data-structures/data-structures_functions_01.py
@@ -0,0 +1,49 @@
+# data structures - https://codechalleng.es/bites/21/
+# Bite 21. Query a nested data structure
+
+#DIRECTIONS
+# Given the provided cars dictionary:
+#  Get all Jeeps
+#  Get the first car of every manufacturer.
+#  Get all vehicles containing the string Trail in their names (should work for other grep too)
+#  Sort the models (values) alphabetically
+
+#More specific directions givin in the  docstrings of functions
+
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models
+       (original order)"""
+    jeep_models = cars["Jeep"]
+    jeeps = ', '.join(jeep_models)
+    return jeeps
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    first_model = [model[0] for model in cars.values()]
+    return first_model
+
+
+def get_all_matching_models(cars=cars, grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    
+
+
+def sort_car_models(cars=cars):
+    """return a copy of the cars dict with the car models (values)
+       sorted alphabetically"""
+    pass
+
+
+print(get_all_jeeps(cars))
+print(get_first_model_each_manufacturer())
