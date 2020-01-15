a712895bf06d813d24dceaf115c3a923903e0094
Dave <dm@Daves-iMac.lan>
2020-01-15 07:32:17 -0500

    Finished basic RPS game

--- scripts/13-15-Text-Based-games/RPS_basic.py
+++ scripts/13-15-Text-Based-games/RPS_basic.py
@@ -1,12 +1,43 @@
+# RPS game at its most basic level.
+# Made TODOs for future tinkering once complete with 100days.
+
+
 import random
 
-options = ["rock", "paper", "scissors"]
+# TODO: Make more efficient
+# TODO: add classes
+# TODO: add function to add help and/or rule set
+# TODO: add function to keep track of score and print out on screen after game over
+# TODO: add function to give player choice to pick best of number in series.
 
 
-comp = random.choice(options)
-user_choice = None
-while user_choice not in options:
-    user_choice= input("Please choose rock, paper, or scissors: ")
 
-print("you: ", user_choice
-      , "VS", comp)
+
+def main():
+    options = ["rock", "paper", "scissors"]
+    comp = None
+    user_choice = None
+    for game in range(15):
+        while user_choice not in options:
+            user_choice= input("Please choose rock, paper, or scissors: ")
+        comp = random.choice(options)
+        print("you: ", user_choice, "VS", comp)
+
+        if user_choice == comp:
+            print("Tie")
+            user_choice = None
+        elif (user_choice == "paper") and (comp == "rock"):
+            print("win")
+            user_choice = None
+        elif (user_choice == "scissors") and (comp == "paper"):
+            print("win")
+            user_choice = None
+        elif (user_choice == "rock") and (comp == "scissors"):
+            print("win")
+            user_choice = None
+        else:
+            print("Loss")
+            user_choice = None
+
+if __name__ == "__main__":
+    main()
