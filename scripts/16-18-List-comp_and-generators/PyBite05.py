36af7df57fc247830bef4a103e98fe61c855a31b
Dave <davemarcinowski@gmail.com>
2020-01-18 23:40:37 -0500

    more python working

---
+++ scripts/16-18-List-comp_and-generators/PyBite05.py
@@ -0,0 +1,31 @@
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    return list(dict.fromkeys(NAMES))
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(NAMES)
+    print(names[0])
+
+
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    # ...
+    pass
+
+
+print(dedup_and_title_case_names(NAMES))
+print()
+print(sort_by_surname_desc(NAMES))
